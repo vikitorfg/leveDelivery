6bff208dfa6e8d597b9bb84c20465126ac77c931
vikitorfg <victorgarzel@hotmail.com>
2020-11-25 15:35:52 -0300

    created seeds for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(title: "Creme de cebola",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                price: rand(20) + 4.99,
+                calories: rand(500) + 150,
+                protein: rand(30),
+                carbohidrates: rand(70),
+                fibers: rand(8),
+                total_fat: rand(30),
+                sat_fat: rand(10),
+                sodium: rand(30)
+)
+
+Product.create!(title: "Marreco com arroz",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                price: rand(20) + 4.99,
+                calories: rand(500) + 150,
+                protein: rand(30),
+                carbohidrates: rand(70),
+                fibers: rand(8),
+                total_fat: rand(30),
+                sat_fat: rand(10),
+                sodium: rand(30)
+)
+
+Product.create!(title: "Carne de panela",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                price: rand(20) + 4.99,
+                calories: rand(500) + 150,
+                protein: rand(30),
+                carbohidrates: rand(70),
+                fibers: rand(8),
+                total_fat: rand(30),
+                sat_fat: rand(10),
+                sodium: rand(30)
+)
+
+Product.create!(title: "Salmao com legumes",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                price: rand(20) + 4.99,
+                calories: rand(500) + 150,
+                protein: rand(30),
+                carbohidrates: rand(70),
+                fibers: rand(8),
+                total_fat: rand(30),
+                sat_fat: rand(10),
+                sodium: rand(30)
+)
