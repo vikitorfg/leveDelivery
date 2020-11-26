aed721d96b856ba64da18c4f55f15ac3e2f29eb0
vikitorfg <victorgarzel@hotmail.com>
2020-11-26 15:53:36 -0300

    added restriction to Product and updated seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,17 @@
 class Product < ApplicationRecord
-    validates_presence_of :title, :description, :price, :calories, :protein, 
+    validates_presence_of :title, :description, :image_url, :price, :calories, :protein, 
                             :carbohidrates, :fibers, :total_fat, 
                             :sat_fat, :sodium
 
+    validates :title, uniqueness: true
+
     validates :price, :calories, :protein, :carbohidrates, :fibers, :total_fat, 
                             :sat_fat, :sodium, numericality: { greater_than_or_equal_to: 0 }
+    
+    validates   :image_url, allow_blank: true, format: {
+                with: %r{\.(gif|jpg|png)\z}i,
+                message: 'must be a URL for GIF, JPG or PNG image.'
+                }
+
 end
+
