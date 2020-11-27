c52e2f1a03d592febf9356607362855fe0afaaae
vikitorfg <victorgarzel@hotmail.com>
2020-11-27 13:59:48 -0300

    fix: added default to LineItem quantity

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,7 +7,6 @@ class Cart < ApplicationRecord
       current_item.quantity += 1
     else
       current_item = line_items.build(product_id: product.id)
-      current_item.quantity = 1
     end
     current_item
   end
