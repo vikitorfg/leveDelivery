e05ad289e667628ac58e0388c4b63969cbaa7004
vikitorfg <victorgarzel@hotmail.com>
2021-01-04 15:51:09 -0300

    implemente add remove item in product card

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
-  before_action :set_line_item, only: %i[show edit update destroy]
+  before_action :set_cart, only: [:create, :destroy]
+  before_action :set_line_item, only: %i[show edit update]
 
   # GET /line_items
   # GET /line_items.json
@@ -29,7 +29,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html {  }
+        # format.js   { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -44,10 +45,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -55,10 +54,15 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    product = Product.find(params[:id])
+    @line_item = @cart.line_items.find_by(product_id: product.id)
+    
+    unless @line_item.nil?
+      @line_item = @cart.remove_product(product)
+    end
+    # product = Product.find(@line_item.product_id)
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { }
     end
   end
 
@@ -66,6 +70,7 @@ class LineItemsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_line_item
+    byebug
     @line_item = LineItem.find(params[:id])
   end
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,6 +11,18 @@ class Cart < ApplicationRecord
     current_item
   end
 
+  def remove_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+
+    if current_item.quantity == 1
+      current_item.destroy
+    else 
+      current_item.quantity -= 1
+      current_item.save
+    end
+
+  end
+
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
